0e4c742fb17883859f9959dfd3a244e9d82f7799
LifeCell-mon <diorlisacenko@gmail.com>
2024-10-15 20:12:53 +0300

    da
    da

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// Скрипт для підсвітки пунктів меню при скролінгу
+const sections = document.querySelectorAll('.content-section');
+const navLinks = document.querySelectorAll('nav ul li a');
+
+window.addEventListener('scroll', () => {
+    let currentSection = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop - 50) {
+            currentSection = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').includes(currentSection)) {
+            link.classList.add('active');
+        }
+    });
+});
